e00dd76b3bb3a43ea8612ae31ea537925032b404
Abdullah <abdullaahmadsheikh@gmail.com>
2024-06-26 11:27:34 +0300

    project setup done

---
+++ TwitterClone/Controllers/Core/NotificationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationViewController.swift
+//  TwitterClone
+//
+//  Created by Abdulla Ahmad on 6/26/24.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
